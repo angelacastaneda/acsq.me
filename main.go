405d102bb6d469e0f6e5b633137d08378bdcbb6e
Ángel Castañeda <angelaxel1420@disroot.org>
2023-07-13 16:14:43 -0500

    Have https ready and built into repo
    I got my let's encrypt certs ready, and I made my site auto redirect
    http->https. This only activates when I use the :443 port in my addr flag to
    make testing with localhost easier.

--- main.go
+++ main.go
@@ -6,16 +6,24 @@ import (
 	"net/http"
 )
 
+const (
+  fullchain = "/etc/letsencrypt/live/angel-castaneda.com/fullchain.pem"
+  privkey = "/etc/letsencrypt/live/angel-castaneda.com/privkey.pem"
+)
+
 func main() {
-  // flag address for function
 	addr := flag.String("addr", ":4000", "HTTP Network Address")
-	flag.Parse()
+	flag.Parse() // required before flag is used
+
+  if *addr == ":443" {
+    scheme = "https"
+  } 
 
 	mux := http.NewServeMux()
 
   // TODO: Make cooler router
 	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("/about", aboutHandler)
+	mux.HandleFunc("/about", pageHandler("about"))
 	mux.HandleFunc("/library", pageHandler("library"))
 	mux.HandleFunc("/todo", pageHandler("todo"))
 	mux.HandleFunc("/friends", pageHandler("friends"))
@@ -25,12 +33,19 @@ func main() {
 	mux.HandleFunc("/tags/", tagHandler)
 	mux.HandleFunc("/favicon.ico", faviconHandler)
 
-
-
 	fileServer := http.FileServer(http.Dir("./static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Starting the server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+  www := wwwRedirect(mux)
+
+  log.Printf("Starting the server on %s", *addr)
+
+  if *addr == ":443" {
+    go http.ListenAndServe(":80", http.HandlerFunc(httpsRedirect))
+    err := http.ListenAndServeTLS(*addr, fullchain, privkey, www) 
+    log.Fatal(err)
+  } else {
+    err := http.ListenAndServe(*addr, www)
+    log.Fatal(err)
+  }
 }
