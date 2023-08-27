63a1e3a2d77aeaabc7c8598876c42bf256cc21d3
Ángel Castañeda <angelaxel1420@disroot.org>
2023-08-26 19:26:22 -0500

    Document sqlite pkg
    This make file is annoying me into taking this stuff seriously. I'll actually
    us this package for other blogs though, so it's definitely good.

--- sqlite/sql.go
+++ sqlite/sql.go
@@ -1,4 +1,4 @@
-// package for creating, reading, and writing to a sqlite database for my blog
+// Package sqlite for creating, reading, and writing to a sqlite database for my blog
 package sqlite
 
 import (
@@ -7,18 +7,22 @@ import (
 	"errors"
 	"log"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3" // blank import so the sql engine can do its thing in init
 )
 
 var (
 	pathToDB       = "./posts.sqlite3"
-	ErrNotComplete = errors.New("unfilled required attributes")
+	errNotComplete = errors.New("unfilled required attributes")
 )
 
-type db struct {
+// DB custom type is gonna be my attempt at making paths to the database
+// defined in the original package. Still a todo for now.
+type DB struct {
 	PathToDB string
 }
 
+// Post type has pretty much all of the content and metadata that I'd need
+// to manipulate for a complex blogging system that I can still manage.
 type Post struct {
 	Title       string
 	FileName    string
@@ -30,18 +34,25 @@ type Post struct {
 	Thumbnail   Img
 }
 
+// Img struct exists for my thumbnail image in the front of my site, but
+// I will definitely use this package for other things that I'll need for
+// other friend's sites I'm designing
 type Img struct {
 	Src   string `json:"src"`
 	Alt   string `json:"alt"`
 	Title string `json:"title"`
 }
 
+// Tag struct is there to aggregate together posts with common themes.
+// I might add an html related attribute like content in post to let
+// tag pages be more than just a paragraph.
 type Tag struct {
 	Name        string
 	Category    string
 	Description string
 }
 
+// OpenDB just opens the connection to the db from the pathToDB package var
 func OpenDB() (db *sql.DB) {
 	db, err := sql.Open("sqlite3", pathToDB)
 	if err != nil {
@@ -50,12 +61,19 @@ func OpenDB() (db *sql.DB) {
 	return db
 }
 
+// CloseDB is the mirror of open. Generally just defer it immediately after
+// OpenDB
 func CloseDB(db *sql.DB) {
 	if db != nil {
 		db.Close()
 	}
 }
 
+// MakeDB makes the basic schema for the database if it doesn't exist.
+// originally, it had a trigger for update_date whenever something changed, but
+// that was too clunky to deal with for me. I might eventually add it searching
+// for a backup .sql file in the same directory as the db in case there's
+// backup data.
 func MakeDB() (err error) {
 	db := OpenDB()
 	defer CloseDB(db)
@@ -98,6 +116,13 @@ func MakeDB() (err error) {
 	return nil
 }
 
+// AggregatePosts gets all the posts and tags metadata from the db into a slice
+// of posts sorted in reverse chron order. if you give it a tag, it'll only
+// return posts with that tag. giving the func a negative number returns an
+// empty slice and giving it zero will return all the entries that match. If
+// you give it an integer less than the total amount of posts available with
+// the filter, it'll return that many posts back in the slice, still in reverse
+// chron order.
 func AggregatePosts(postQty int, filterTag string) (posts []Post, err error) {
 	if postQty < 0 {
 		return []Post{}, nil
@@ -149,6 +174,8 @@ func AggregatePosts(postQty int, filterTag string) (posts []Post, err error) {
 	return posts, nil
 }
 
+// FetchPost brings back the struct data of a single post including a tag slice
+// of all matching tags to post in associative identity.
 func FetchPost(fileName string) (post Post, err error) {
 	if !DoesPostExist(fileName) {
 		return Post{}, errors.New(fileName + " doesn't exist")
@@ -205,6 +232,12 @@ func FetchPost(fileName string) (post Post, err error) {
 	return post, nil
 }
 
+// FetchThumbnail is for a very niche thing I needed for my home page that
+// displays my latest photos post with a valid thumbnail. Sqlite doesn't have
+// structs, and I don't wanna bother with pgsql or mariadb for something this
+// small, so I just marshal and unmarshal json into the db as text. This func
+// fetches just that post and gives back not only the img struct but the rest
+// of the post as well for easily being able to link back to the post.
 func FetchThumbnail() (post Post, err error) {
 	db := OpenDB()
 	defer CloseDB(db)
@@ -233,6 +266,8 @@ func FetchThumbnail() (post Post, err error) {
 	return post, nil
 }
 
+// FetchTag is basically identical to FetchPost but way smaller and less
+// complicated
 func FetchTag(tagName string) (tag Tag, err error) {
 	db := OpenDB()
 	defer CloseDB(db)
@@ -247,6 +282,8 @@ func FetchTag(tagName string) (tag Tag, err error) {
 	return tag, nil
 }
 
+// DoesPostExist is an evolution from my original filesystem func that I still
+// use called doesFileExist that ensures a certain post in actually in the db.
 func DoesPostExist(fileName string) bool {
 	db := OpenDB()
 	defer CloseDB(db)
@@ -263,6 +300,7 @@ func DoesPostExist(fileName string) bool {
 	return count > 0
 }
 
+// DoesTagExist is the exact same deal as DoesPostExist
 func DoesTagExist(tag string) bool {
 	db := OpenDB()
 	defer CloseDB(db)
@@ -279,6 +317,9 @@ func DoesTagExist(tag string) bool {
 	return count > 0
 }
 
+// checkPost is an internal func to ensure that all the attributes in a post
+// that are NOT NULL in the db are, in fact, not null before loading them into
+// the db.
 func checkPost(p Post) error { // don't need to check thumbnail
 	if p.Title == "" ||
 		p.FileName == "" ||
@@ -287,20 +328,27 @@ func checkPost(p Post) error { // don't need to check thumbnail
 		p.PubDate == "" ||
 		p.UpdateDate == "" ||
 		len(p.Tags) < 1 {
-		return ErrNotComplete
+		return errNotComplete
 	}
 	return nil
 }
 
+// checkTag is idential but smaller
 func checkTag(t Tag) error {
 	if t.Name == "" ||
 		t.Category == "" ||
 		t.Description == "" {
-		return ErrNotComplete
+		return errNotComplete
 	}
 	return nil
 }
 
+// AddPost checks to see if a post is valid first will checkPost then also by
+// ensuring every tag that is inside the post exists in the db. You don't even
+// need to add anything more than the names of the tags in the tag slice of the
+// post as that's how it's checked before the post is inserted into the db as
+// well as being how the tag_id is filled into the posts_tags associative
+// identity for linking the metadata together.
 func AddPost(post Post) (err error) {
 	if err = checkPost(post); err != nil {
 		return err
@@ -346,6 +394,9 @@ func AddPost(post Post) (err error) {
 	return nil
 }
 
+// AddTag is just a simpler version of AddPost. The only filtering done before
+// is seeing if your tag struct has all the required attributes to add to the
+// db.
 func AddTag(tag Tag) (err error) {
 	if err = checkTag(tag); err != nil {
 		return err
@@ -365,6 +416,7 @@ func AddTag(tag Tag) (err error) {
 	return nil
 }
 
+// DeletePost is incomplete
 func DeletePost(fileName string) (err error) {
 	if !DoesPostExist(fileName) {
 		return errors.New(fileName + " doesn't exist")
@@ -382,6 +434,7 @@ func DeletePost(fileName string) (err error) {
 	return nil
 }
 
+// DeleteTag is incomplete
 func DeleteTag(tagName string) (err error) {
 	if !DoesTagExist(tagName) {
 		return errors.New(tagName + " doesn't exist")
