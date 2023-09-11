5eade679db0ad86ee64abf1631241e77332614eb
Ángel Castañeda <angelaxel1420@disroot.org>
2023-09-11 00:04:40 -0500

    Make compress.sh work
    downscale to 720 -> lower qual to 72% -> webp

--- static/img/compress.sh
+++ static/img/compress.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-og_image="$1"
-echo "${og_image%.*}"
+res="1280x720"
+qual="72"
+
+og_img="$1"
+pic_root="${og_img%.*}"
+
+convert "$og_img" -resize "$res"\> comp_"$og_img"
+convert comp_"$og_img" -quality "$qual"% comp_"$og_img"
+
+cwebp comp_"$og_img" -o "$pic_root.webp"
+
+rm -fv comp_"$og_img"
+
+du -h "$pic_root"*
