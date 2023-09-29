a089b7263eb0649add91e4a6621f9aadc2b32036
Ángel Castañeda <angel@acsq.me>
2023-09-29 15:50:12 -0500

    Fmt out whitespace

--- cmd/http/langs.go
+++ cmd/http/langs.go
@@ -208,7 +208,7 @@ var dictionary = buildDictionary([]map[string]string{
 	},
 })
 
-// this awesome func come from a stack overflow post, and is not my own 
+// this awesome func come from a stack overflow post, and is not my own
 // https://stackoverflow.com/a/76735864/21316874 ; it falls under CC BY-SA 4.0
 func buildDictionary(maps []map[string]string) map[string]map[string]string {
 	rosetta := make(map[string]map[string]string)
